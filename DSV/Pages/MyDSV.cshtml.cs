a5ce34bea8878c073581b801b21457109c799006
Eylem003 <eyy001@edu.zealand.dk>
2025-04-04 13:46:30 +0200

    Booking system

--- DSV/Pages/MyDSV.cshtml.cs
+++ DSV/Pages/MyDSV.cshtml.cs
@@ -1,3 +1,30 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
-namespace DSV.Pages;
+public class Booking
+{
+    public string room { get; set; }
+    public DateTime BookingDate { get; set; }
+    public static string BookingTime { get; set; }
+    public string Comment { get; set; }
+
+    public class BookingController : Controller
+    {
+        private static List<Booking> bookings = new List<Booking>();
+
+
+        public IActionResult BookRoom(string room)
+        {
+            ViewBag.Room = room;
+            return View();
+        }
+
+        public IActionResult SubmitBooking(string room, DateTime BookingDate, string BookingTime, string comment)
+        {
+            if (string.IsNullOrEmpty(room) || string.IsNullOrEmpty(BookingTime) || string.IsNullOrEmpty(comment))
+                return View("Error", new { message = "Alle felter skal udfyldes." });
+        }
+    }
+}
